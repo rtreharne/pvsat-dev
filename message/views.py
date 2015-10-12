d3b87f9292c98fb7d4a8fe36b0ad54f1c45832a8
Rob <R.Treharne@liverpool.ac.uk>
2015-10-12 16:15:52 +0100

    lots of css changes + programme.html page2

--- message/views.py
+++ message/views.py
@@ -5,18 +5,19 @@ from django.conf import settings
 
 def message(request):
 
-	sent = False
+    sent = False
 
-	if request.method == 'POST':
-		contact_form = MessageForm(data=request.POST)
+    if request.method == 'POST':
+        contact_form = MessageForm(data=request.POST)
 
-		if contact_form.is_valid():
-			message = contact_form.save()
-			sent = True
+        if contact_form.is_valid():
+            message = contact_form.save()
+            sent = True
+
             send_mail(request.POST['subject'], request.POST['message'], '', [settings.ADMIN_EMAIL], fail_silently=False)
-		else:
-			print contact_form.errors
-	else:
-		contact_form = MessageForm()
-	
-        return render(request, 'message.html', {'contact_form': contact_form, 'sent': sent})
+        else:
+            print contact_form.errors
+    else:
+        contact_form = MessageForm()
+
+    return render(request, 'message.html', {'contact_form': contact_form, 'sent': sent})
