4db953bc21df2a4e48150c4c12b6833bd48b9144
Rob <R.Treharne@liverpool.ac.uk>
2015-10-12 10:11:57 +0100

    SITE_URL added to settings.py

---
+++ message/migrations/0053_auto_20151012_0902.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('message', '0052_auto_20151010_0708'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='message',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 12, 9, 2, 51, 908534)),
+        ),
+    ]
