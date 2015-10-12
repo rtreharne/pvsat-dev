98d2ab9c311f6acf76b5e04a7e350c3eaf9ab6fd
Rob <R.Treharne@liverpool.ac.uk>
2015-10-12 13:03:06 +0100

    restart after file backup

---
+++ message/migrations/0054_auto_20151012_1159.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('message', '0053_auto_20151012_0902'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='message',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 12, 11, 59, 43, 143736)),
+        ),
+    ]
