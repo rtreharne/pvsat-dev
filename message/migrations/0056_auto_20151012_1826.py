f3d32046a9ad9907611d50f0d4a006dd4620dc31
Robert <R.Treharne@liverpool.ac.uk>
2015-10-13 06:31:51 +0100

    homepage tweak to sponsors

---
+++ message/migrations/0056_auto_20151012_1826.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('message', '0055_auto_20151012_1221'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='message',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 12, 18, 26, 25, 652580)),
+        ),
+    ]
