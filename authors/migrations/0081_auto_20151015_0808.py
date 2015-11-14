78e41c78156c0f189f82cf63002b5911d2d1d32f
treharne <cdtnspv@gmail.com>
2015-11-14 12:10:17 +0000

    update

---
+++ authors/migrations/0081_auto_20151015_0808.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authors', '0080_auto_20151015_0807'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='abstract',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 15, 8, 8, 7, 298043)),
+        ),
+    ]
---
+++ message/migrations/0066_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('message', '0065_auto_20151017_1200'),
+        ('message', '0061_auto_20151015_0808'),
+    ]
+
+    operations = [
+    ]
