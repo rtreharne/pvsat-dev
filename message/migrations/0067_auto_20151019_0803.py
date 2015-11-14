115ff529c3ddefc67755f6c98660d55f137012af
Robert <R.Treharne@liverpool.ac.uk>
2015-11-14 12:08:40 +0000

    guest speaker resize box

---
+++ message/migrations/0067_auto_20151019_0803.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('message', '0066_merge'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='message',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 19, 8, 3, 8, 870839)),
+        ),
+    ]
