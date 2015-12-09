659ce5ea8e1abd95f96f9dbf7cd99f3bd54f1fc1
treharne <R.Treharne@liverpool.ac.uk>
2015-12-09 21:18:30 +0000

    allow pdf, fix dashboard css

---
+++ authors/migrations/0089_auto_20151209_2113.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authors', '0088_merge'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='abstract',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 12, 9, 21, 13, 46, 390466)),
+        ),
+    ]
