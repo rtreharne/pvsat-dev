beb452119bb4a193901a27f66dfd81a6a2cc5a52
treharne <R.Treharne@liverpool.ac.uk>
2015-12-16 17:41:17 +0000

    affiliation added to abstract.admin

--- authors/admin.py
+++ authors/admin.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import User
 
 
 class AbstractAdmin(admin.ModelAdmin):
-    list_display = ( 'short_title', 'view_id', 'view_link', 'status', 'delivery','delivery_decision', 'date', 'author_registered')
+    list_display = ( 'short_title', 'view_id', 'view_link', 'affiliation', 'status', 'delivery','delivery_decision', 'date', 'author_registered')
+
+    def affiliation(self, obj):
+        return ("%s" % obj.author.affiliation)
+    affiliation.admin_order_field='author__affiliation'
 
     def short_title(self, obj):
         return ("%s ..." % (obj.title[:50]))
