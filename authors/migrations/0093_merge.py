f1188737668a6e002829cd2aebcd7a9866ef5c90
treharne <R.Treharne@liverpool.ac.uk>
2016-01-19 20:28:39 +0000

    proceedings user pic update

---
+++ authors/migrations/0093_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authors', '0092_auto_20151209_2133'),
+        ('authors', '0091_auto_20151217_1034'),
+    ]
+
+    operations = [
+    ]
---
+++ message/migrations/0073_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('message', '0072_auto_20151209_2133'),
+        ('message', '0071_auto_20151217_1034'),
+    ]
+
+    operations = [
+    ]
