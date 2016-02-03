380506d6a5f82906f704802dae7f55a93a4561fa
treharne <R.Treharne@liverpool.ac.uk>
2016-02-03 23:04:41 +0000

    blog update

---
+++ message/migrations/0083_auto_20160203_2300.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('message', '0082_auto_20160203_1445'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='message',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2016, 2, 3, 23, 0, 3, 457395)),
+        ),
+    ]
