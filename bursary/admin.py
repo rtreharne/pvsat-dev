8bf6217a70be6c3592df3edeaf8f22540a676895
treharne <R.Treharne@liverpool.ac.uk>
2016-02-04 14:08:15 +0000

    bursary admin update

--- bursary/admin.py
+++ bursary/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from bursary.models import Applicant
 
 
-admin.site.register(Applicant)
+class ApplicantAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'view_email', 'institution', 'supervisor', 'status')
+
+    def view_email(self, obj):
+        return u"<a href='mailto:%s'>%s</a>" % (obj.email, obj.email)
+    view_email.allow_tags = True
+    view_email.short_description = 'email'
+
+admin.site.register(Applicant, ApplicantAdmin)
 
 
